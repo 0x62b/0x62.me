e1bfba8374df6b93fd3df45f4f254b1cc6a6e56a
Benjamin Yang <benjamin.yang12@outlook.com>
2025-11-07 15:13:18 +1100

    add language param to cmd parser for about page

--- static/js/parser.js
+++ static/js/parser.js
@@ -1,12 +1,37 @@
 function parse(command) {
-  switch(command) {
+  switch(command[0]) {
     case "help":
     case "?":
-      return "Available commands: help ? ls about";
+      return "Available commands: help ? ls about projects";
     case "ls":
-      return "about.html index.html";
+      return "about.html about-de.html about-zh.html index.html projects.html";
     case "about":
-      location.href = 'about.html';
+      if (command.length > 1) {
+        switch(command[1]) {
+          case 'zh':
+          case 'cn':
+          case 'chinese':
+          case '中文':
+            location.href = `about-zh.html`;
+            break;
+          case 'de':
+          case 'deutsch':
+            location.href = 'about-de.html';
+            break;
+          case 'en':
+          case 'eng':
+          case 'english':
+            location.href = 'about.html';
+            break;
+          default:
+            return "parameter 1 invalid";
+        }
+      }
+      else location.href = 'about.html';
+      break;
+    case "projects":
+      location.href = 'projects.html';
+      break;
     default:
       return "Command does not exist."
   }
@@ -15,7 +40,7 @@ function parse(command) {
 function command() {
   let box = document.getElementById("cmd-input");
   let cmd = box.value;
-  let ret = parse(cmd);
+  let ret = parse(cmd.split(' '));
   document.getElementById("term-output").innerText += "visitor@0x62.me ~ $ " + cmd + "\n";
   document.getElementById("term-output").innerText += (ret + "\n");
   box.value = "";
