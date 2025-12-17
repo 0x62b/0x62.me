b28f51ff5e204dff9093abddb43f263bae1b8429
Benjamin Yang <benjamin.yang12@outlook.com>
2025-12-17 16:28:50 +1100

    use reader in redirect

--- static/js/commands/redirect.js
+++ static/js/commands/redirect.js
@@ -3,19 +3,19 @@ export async function redirect(page, command=null) {
     switch(command[1]) {
       case 'zh':
       case '中文':
-        location.href = `zh/${page}`;
+        location.href = `/reader.html?page=zh/${page}`;
         break;
       case 'de':
       case 'deutsch':
-        location.href = `de/${page}`;
+        location.href = `/reader.html?page=de/${page}`;
         break;
       case 'en':
       case 'english':
-        location.href = page;
+        location.href = `/reader.html?page=en/${page}`;
         break;
       default:
         return '<span class="text-red">parameter 1 invalid</span>';
     }
   }
-  else location.href = page;
+  else location.href = `/reader.html?page=en/${page}`;
 }
