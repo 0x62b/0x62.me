5d9d97e7431b9e0b647485a1a4bfa8473f7d55e9
Benjamin Yang <benjamin.yang12@outlook.com>
2025-12-17 22:36:46 +1100

    more localisations

--- static/js/commands/redirect.js
+++ static/js/commands/redirect.js
@@ -3,19 +3,19 @@ export async function redirect(page, command=null) {
     switch(command[1]) {
       case 'zh':
       case '中文':
-        location.href = `/reader.html?page=zh/${page}`;
+        location.href = `/reader.html?page=content/${page}&lang=zh`;
         break;
       case 'de':
       case 'deutsch':
-        location.href = `/reader.html?page=de/${page}`;
+        location.href = `/reader.html?page=content/${page}&lang=de`;
         break;
       case 'en':
       case 'english':
-        location.href = `/reader.html?page=en/${page}`;
+        location.href = `/reader.html?page=content/${page}&lang=en`;
         break;
       default:
         return '<span class="text-red">parameter 1 invalid</span>';
     }
   }
-  else location.href = `/reader.html?page=en/${page}`;
+  else location.href = `/reader.html?page=content/${page}&lang=en`;
 }
