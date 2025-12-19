64b6c54ff80a6ec029eaf9ab9ad699f3f3c4554f
Benjamin Yang <benjamin.yang12@outlook.com>
2025-12-19 15:02:02 +1100

    python backend server
    hopefully no leakies

---
+++ backend/main.py
@@ -0,0 +1,56 @@
+from dotenv import load_dotenv
+from flask import Flask, jsonify
+
+import requests
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/api/slack_online', methods=['GET'])
+def slack_online():
+    token = os.environ.get('SLACK_TOKEN')
+    user_id = "U092839T3A7"
+    
+    if not token:
+        return jsonify({"error": "SLACK_TOKEN not set"}), 500
+
+    response = requests.post(
+        "https://slack.com/api/users.getPresence",
+        data={
+            "token": token,
+            "user": user_id
+        }
+    )
+    
+    return jsonify(response.json()) #presence
+
+@app.route("/api/slack_status", methods=['GET'])
+def slack_status():
+    token = os.environ.get('SLACK_TOKEN')
+    user_id = "U092839T3A7"
+    
+    if not token:
+        return jsonify({"error": "SLACK_TOKEN not set"}), 500
+
+    response = requests.post(
+        "https://slack.com/api/users.profile.get",
+        data={
+            "token": token,
+            "user": user_id
+        }
+    )
+    
+    return jsonify(response.json()) # profile.status_text
+
+@app.route('/api/spotify', methods=['GET'])
+def spotify_status():
+    return jsonify({})
+
+@app.route('/api/hackatime', methods=['GET'])
+def hackatime_status():
+    return jsonify({})
+
+if __name__ == '__main__':
+    app.run(debug=True)
