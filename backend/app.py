31b9a89fe34b2eccff5097ae53f7a0e3380f0774
Benjamin Yang <benjamin.yang12@outlook.com>
2025-12-20 19:25:34 +1100

    integrate status page

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 import requests
 import os
@@ -7,6 +8,7 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/slack_online', methods=['GET'])
 def slack_online():
