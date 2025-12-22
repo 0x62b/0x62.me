8f0d159c21976408def11958533e2395d62ba3cd
Benjamin Yang <benjamin.yang12@outlook.com>
2025-12-22 12:55:07 +1100

    creating new posts

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,12 @@
 from dotenv import load_dotenv
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+from pathlib import Path
 
+import re
 import requests
 import os
+import json
 
 load_dotenv()
 
@@ -53,6 +56,53 @@ def spotify_status():
 @app.route('/api/hackatime', methods=['GET'])
 def hackatime_status():
     return jsonify({})
+
+@app.route('/api/posts/new', methods=['POST'])
+def new_post():
+    title = request.form.get("title")
+    content = request.form.get("content")
+    description = request.form.get("description")
+    timestamp = request.form.get("timestamp")
+    
+    filename = re.sub(r'[^\w\s-]', '', title.lower())
+    filename = re.sub(r'[-\s]+', '-', filename).strip('-')
+    filename = f"{filename}.md"
+    
+    main_dir = Path(__file__).parent.parent / "blog"
+    post_dir = main_dir / "posts"
+    
+    file = post_dir / filename
+    json_file = main_dir / "posts.json"
+    
+    if file.exists():
+        return jsonify({"error": "post with same title already exists"}), 409
+    
+    try:
+        with open(file, 'w') as f:
+            f.write(f"# {title}\n\n{content}")
+        
+        with open(json_file, 'r+') as f:
+            post = {
+                "title": title,
+                "description": description,
+                "filename": filename,
+                "timestamp": int(timestamp)
+            }
+            posts = json.loads(f.read())
+            
+            posts['posts'].append(post)
+            
+            f.seek(0)
+            f.write(json.dumps(posts))
+            f.truncate()
+
+        return jsonify({
+            "success": True,
+            "filename": filename,
+            "path": str(file)
+        }), 201
+    except Exception as e:
+        return jsonify({"error": f"{str(e)}"}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
